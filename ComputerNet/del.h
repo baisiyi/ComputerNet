a99d83ba7c418df19d84240903f7f8d3e7920094
2455138283 <2455138283@qq.com>
2018-12-03 18:58:03 +0800

    baisiyi

--- ComputerNet/del.h
+++ ComputerNet/del.h
@@ -1,1 +1,4 @@
-#pragma once
+#include<iostream>
+using namespace std;
+int DeleteVertex(int chart[][100], int length);
+void DeleteEdge(int chart[][100]);
--- ComputerNet/read.h
+++ ComputerNet/read.h
@@ -1,1 +1,3 @@
-#pragma once
+#include<iostream>
+using namespace std;
+void ReadFile(int chart[][100], int length);
